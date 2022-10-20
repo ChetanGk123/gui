88de002b52e426686f1f51fabe196de26d2d2487
CHETAN G <80114842+ChetanGk123@users.noreply.github.com>
2022-10-20 06:27:26 +0100

    delete old files. skote initial files

--- src/app/shared/interfaces/api-response.ts
+++
@@ -1,7 +0,0 @@
-export interface ApiResponse {
-    result?:boolean
-    message?:string 
-    status?:string 
-    icon?:string 
-    data?:any
-}
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  // apiUrl: 'https://stagespace.tech/school/api/v1/',
-  apiUrl: './api/v1/',
-  production: true,
-  loc: window.location.origin + "/app/",
-  encPassword : "enc_RoyalPalace_Lkp" 
-};
